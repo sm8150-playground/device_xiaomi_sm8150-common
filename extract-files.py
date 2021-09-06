f280ff125745107fb141c666bba6bd945ce7eb3d
kubersharma001 <kubersharma001@gmail.com>
2021-09-06 10:54:12 +0000

    sm8150-common: Switch to AOSP WFD
    Change-Id: I1ae32e41ea3cdb1f3b5d7f81a57f548fa211c9ca

--- extract-files.py
+++ extract-files.py
@@ -38,26 +38,12 @@ lib_fixups: lib_fixups_user_type = {
         'com.qualcomm.qti.dpm.api@1.0',
         'libmmosal',
         'vendor.qti.hardware.fm@1.0',
-        'vendor.qti.hardware.wifidisplaysession@1.0',
         'vendor.qti.imsrtpservice@3.0',
     ): lib_fixup_vendor_suffix,
 }
 
 
 blob_fixups: blob_fixups_user_type = {
-            'system_ext/lib64/libwfdnative.so': blob_fixup()
-                .add_needed('libbinder_shim.so')
-                .add_needed('libinput_shim.so'),
-            'system_ext/lib/libwfdmmsrc_system.so': blob_fixup()
-                .add_needed('libgui_shim.so'),
-            'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
-                .add_needed('libgui_shim.so'),
-            'system_ext/lib/libwfdservice.so': blob_fixup()
-                .add_needed('libaudioclient_shim.so')
-                .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
-            'system_ext/lib64/libwfdservice.so': blob_fixup()
-                .add_needed('libaudioclient_shim.so')
-                .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
             'vendor/etc/init/init.mi_thermald.rc': blob_fixup()
                 .regex_replace(r'seclabel u:r:mi_thermald:s0', ''),
             'vendor/lib64/libwvhidl.so': blob_fixup()
