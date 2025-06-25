9048feabb6d96ea5044248984dedf7c018c10c7c
John Galt <johngaltfirstrun@gmail.com>
2025-06-25 23:35:42 +0530

    sm8150-common: extract-files: patch libwfdservice for libaudioclient_shim
    Change-Id: I6df976f7ef4068ae13f2da267d862a81baac360d
    Signed-off-by: grepfox <grepfox@tutamail.com>

--- extract-files.py
+++ extract-files.py
@@ -54,8 +54,10 @@ blob_fixups: blob_fixups_user_type = {
             'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
                 .add_needed('libgui_shim.so'),
             'system_ext/lib/libwfdservice.so': blob_fixup()
+                .add_needed('libaudioclient_shim.so')
                 .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
             'system_ext/lib64/libwfdservice.so': blob_fixup()
+                .add_needed('libaudioclient_shim.so')
                 .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
             'vendor/etc/init/init.mi_thermald.rc': blob_fixup()
                 .regex_replace(r'seclabel u:r:mi_thermald:s0', ''),
