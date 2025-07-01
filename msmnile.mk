1ded39523ed33e40cde46bc6de7270aad51df6ce
grepfox <grepfox@tutamail.com>
2025-07-02 03:03:53 +0530

    sm8150-common: Don't explicity build telephony packages
    * as per https://github.com/yaap/vendor_yaap/commit/4f44cd82453af2eccebd9db9444f635a14626d0b
    
    Change-Id: I20f032deae635c7fae011eb7b7ab40c40c2214d8

--- msmnile.mk
+++ msmnile.mk
@@ -370,12 +370,6 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
-    ims-ext-common \
-    ims_ext_common.xml \
-    qti-telephony-hidl-wrapper \
-    qti_telephony_hidl_wrapper.xml \
-    qti-telephony-utils \
-    qti_telephony_utils.xml \
     xiaomi-telephony-stub
 
 PRODUCT_BOOT_JARS += \
